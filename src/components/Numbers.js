fea7b19179931db5929abeb00dce42e4a0e27ccb
EvoFox <foulkes.joshua@live.co.uk>
2022-06-08 10:43:41 +0100

    create number buttons

---
+++ src/components/Numbers.js
@@ -0,0 +1,26 @@
+const Numbers = () => {
+	const buttons = [
+		"9",
+		"8",
+		"7",
+		"6",
+		"5",
+		"4",
+		"3",
+		"2",
+		"1",
+		"c",
+		"0",
+		".",
+		"=",
+	];
+	return (
+		<div>
+			{buttons.map((button) => (
+				<button key={button}>{button}</button>
+			))}
+		</div>
+	);
+};
+
+export default Numbers;
